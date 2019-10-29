3356540d1a0c85046ec201b424e7770e19eed920
Collin <cojo2657@colorado.edu>
2019-10-29 10:55:30 -0600

    formatting change

--- views/actions/settings_action.php
+++ views/actions/settings_action.php
@@ -1,16 +1,19 @@
 <?php
 	$phpUsername = $_SESSION['user'];
 	if(isset($_POST['send'])){
+	
 		include'connect_action.php';
-
 		if($_POST['newUsername'] != ""){
+
 			$newUsername = $_POST['newUsername'];
 			$query = "UPDATE users 
 					  	SET usernames = '$newUsername' 
     					WHERE usernames = '$phpUsername'";
-
 			mysqli_query($conn, $query);
+
+			// update the session username
 			$_SESSION['user'] = $newUsername;
+			// update the php username so the js can populate the 	   phlaceholder with new username
 			$phpUsername = $newUsername;
 		}
 	}	
