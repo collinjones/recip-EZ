22067816cac63c82cd78e33e67b242d6aa36277f
Collin <cojo2657@colorado.edu>
2019-11-11 12:06:43 -0700

    fix

--- views/actions/homepage_checkbox_action.php
+++ views/actions/homepage_checkbox_action.php
@@ -1,17 +1,17 @@
 <?php
 	include 'connect_action.php';
 
-	$query = "SELECT IngredientName FROM IngredientName WHERE ingredienttype = 1";
-	$result = mysqli_query($conn, $query);
+	$query_meat = "SELECT IngredientName, IngredientCode FROM IngredientName WHERE ingredienttype = 1";
+	$meat_result = mysqli_query($conn, $query_meat);
 
 
 	if($_SESSION['vegetarian'] == 0){
-		if(mysqli_num_rows($result) > 0){
+		if(mysqli_num_rows($meat_result) > 0){
 			echo '<h4>MEATS</h4>';
 			echo '<div style="padding: 5%" class="form-row">';
-			while($row = mysqli_fetch_array($result)){
+			while($row = mysqli_fetch_array($meat_result)){
 				echo '<div style="margin-left: 5px;" class="custom-control custom-checkbox">';
-				echo '<input name="check_list[]" type="checkbox" class="custom-control-input" id="'. strtolower($row['IngredientName']) .'">';
+				echo '<input name="check_list[]" value="'. $row['IngredientCode'].'"type="checkbox" class="custom-control-input" id="'. strtolower($row['IngredientName']) .'">';
 				echo '<label class="custom-control-label" for="' . strtolower($row['IngredientName']) . '">'. $row['IngredientName'] .'</label>';
 				echo '</div>';
 			}
@@ -26,4 +26,5 @@
 				</div></div>';
 		echo '<br>';
 	}
+
 ?>
